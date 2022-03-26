124b06d18bfdfc2e11991802fce75d7232fe55b3
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2022-03-26 12:03:19 -0300

    Adicionar arquivos de projeto.

---
+++ Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,37 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "6rgt33399999grtrt02f",
+                    Name = "IPhone X",
+                    Description = "lalalal",
+                    Image = null,
+                    Price = 950,
+                    Category = "Smartphone"
+                },
+            };
+        }
+    }
+}
